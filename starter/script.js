231a9d6ac2f4c2b2cb83301523cee525dfa2bb3f
Dominik Zglinski <d.zglinski@clickray.pl>
2022-03-14 21:52:47 +0100

    switching player added 50%

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0E = document.querySelector('.player--0');
+const player1E = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El  = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -11,10 +13,12 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0]; 
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting conditions
-score1El.textContent = 0;
+score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
@@ -30,14 +34,17 @@ btnRoll.addEventListener('click', function(){
     if(dice !== 1){
         // Add dice to current score
         currentScore += dice;
-        current0El.textContent = currentScore; // CHANGE LATER to display for current player
-
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }
     else
     {
         currentScore = 0;
-        current0El.textContent = currentScore;
-        // Switch to next plater
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+
+        player0E.classList.toggle('player--active');
+        player1E.classList.toggle('player--active');
+        // Switch to next player
     }
-})
+}) 
 
